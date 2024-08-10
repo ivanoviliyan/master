dfba4d9d732151d196fda2fb8e1f57645a482e24
Iliyan Ivanov <iliyan.ivanov_mail@yahoo.com>
2024-08-10 12:33:47 +0300

    new changes adding react router dom

---
+++ frontend/my-react-app/src/components/Singup/Singup.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Singup.css'
+
+const Singup = () => {
+
+  return (
+    <>
+        <div className="singup-form">
+            <div className="buttons">
+                <button className="singup">Sing up</button>
+                <button className="cancel">Cancel</button>
+            </div>
+        </div>
+    </>
+  )
+}
+
+
+export default Singup
